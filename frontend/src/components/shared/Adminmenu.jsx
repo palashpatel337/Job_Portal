bd10a5f59ca3b2625ec7da54893a2dff409ded89
Palash Patel <palzz2469@gmail.com>
2026-04-01 11:42:06 +0530

    Fix: admin dashboard

--- frontend/src/components/shared/Adminmenu.jsx
+++ frontend/src/components/shared/Adminmenu.jsx
@@ -264,7 +264,7 @@ const mainNav = [
   { title: "Create Company",  url: "/dashboard/recruiter/create-company", icon: PlusCircle      },
   { title: "Post Job",        url: "/dashboard/recruiter/post-job",       icon: PlusCircle      },
   { title: "My Jobs",         url: "/dashboard/recruiter/my-jobs",        icon: Briefcase       },
-  { title: "Applications",    url: "/dashboard/recruiter/applicants",     icon: Users           },
+  { title: "Applications",    url: `/dashboard/recruiter/my-jobs/${jobId}/applicants`,     icon: Users           },
   ];
 
 const bottomNav = [
