990b016e06f0e016a1a1d3db5549a9b7e1778f7e
Palash Patel <palzz2469@gmail.com>
2026-04-03 23:15:49 +0530

    Fix: admin dashboard frontend

--- frontend/src/pages/admin/CreateCompany.jsx
+++ frontend/src/pages/admin/CreateCompany.jsx
@@ -347,7 +347,7 @@ function CreateCompany() {
                   onChange={(e) => setName(e.target.value)}
                   placeholder="Enter company name"
                   required
-                  className="lg:w-[40vw] bg-transparent text-white border-white/10 focus:border-purple-400 focus:ring-purple-500/30"
+                  className="lg:w-[50vw] lg:h-8 bg-transparent text-white border-white/10 focus:border-purple-400 focus:ring-purple-500/30"
                 />
               </div>
 
