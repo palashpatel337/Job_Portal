5abe4bec051d250c018b374cd6a36b40d4ee8bdb
Palash Patel <palzz2469@gmail.com>
2026-04-14 17:26:42 +0530

    Fix: adding save jobs feature

--- frontend/src/pages/user/SavedJobs.jsx
+++ frontend/src/pages/user/SavedJobs.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Layout from "../components/shared/Layout";
+import Layout from "@/components/shared/Layout";
 import { useAuth } from "@/context/Auth";
 
 function SavedJobs() {
